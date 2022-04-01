e282f90804ed66c585a3245f555cba14f161e75a
jakesfcoetz <jakesfcoetz@gmail.com>
2022-04-01 08:08:24 +0200

    rename training-wheels to app

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'training-wheels'`, () => {
+  it(`should have as title 'app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('training-wheels');
+    expect(app.title).toEqual('app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('training-wheels app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('app app is running!');
   });
 });
